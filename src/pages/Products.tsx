f0361f7f4f5d84abb9d209c50e1d6b7fd194a1c4
Yureks Solutions <yureksolutions@gmail.com>
2026-04-15 14:45:27 +0530

    Update product grid to single column layout on mobile

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -190,7 +190,7 @@ const Products = () => {
             {/* Products Grid */}
             {paginatedItems.length > 0 ? (
               <>
-                <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 lg:gap-6">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-6">
                   {paginatedItems.map((product, index) => (
                     <div 
                       key={product.id}
